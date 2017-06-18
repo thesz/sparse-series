3e614b929f79f4f97e5ee7c6a1f9520032c2c888
thesz <sergueyz@gmail.com>
2017-06-19 01:03:18 +0300

    Some combinators

--- SparseSe.hs
+++ SparseSe.hs
@@ -6,6 +6,9 @@
 --
 -- Copyright (C) 2017 Serguey Zefirov
 
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS -Wno-tabs #-}
+
 module SparseSe where
 
 import qualified Data.Map.Strict as Map
@@ -23,9 +26,21 @@ data Sparse a =
 -- |Sparse series.
 -- 
 data SpSe a = SpSe {
-	  spseCurrentIndex	:: !Int		-- |Row index.
+	  spseCurrentRowIndex	:: !Int		-- |Row index.
 	, spseSparses		:: [Sparse a]	-- |log structured sequence.
-	, spseMap		:: !(Map.Map (Int, Int) a)	-- |Top-level cache.
+	, spseCache		:: !(Map.Map (Int,Int) a)	-- |Cache for several last rows.
+	, spseRow		:: !(Map.Map Int a)	-- |Current row cache
 	}
 	deriving (Show)
 
+empty :: SpSe a
+empty = SpSe 0 [] Map.empty Map.empty
+
+insertToRow :: (Eq a, Num a) => Int -> a -> SpSe a -> SpSe a
+insertToRow k a spse@(SpSe{..})
+	| a == 0 = spse
+	| otherwise = SpSe spseCurrentRowIndex spseSparses spseCache $
+		Map.insert k a spseRow
+
+newRow :: SpSe a -> SpSe a
+newRow spse@(SpSe{..}) = undefined
